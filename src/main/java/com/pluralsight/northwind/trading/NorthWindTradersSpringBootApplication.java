6e1844fa87e59b8ecb49ae765cb8c294492987c8
Romell Walker <walkerromell@gmail.com>
2025-12-09 11:58:26 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/northwind/trading/NorthWindTradersSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.northwind.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthWindTradersSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthWindTradersSpringBootApplication.class, args);
+	}
+
+}
