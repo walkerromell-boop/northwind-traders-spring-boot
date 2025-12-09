1ee91725e29ed7a999ba55896f3a42d7e0565927
Romell Walker <walkerromell@gmail.com>
2025-12-09 14:25:16 -0500

    added interface classes

--- src/main/java/com/pluralsight/northwind/trading/UI/UserInterface.java
+++ src/main/java/com/pluralsight/northwind/trading/UI/UserInterface.java
@@ -1,4 +1,19 @@
 package com.pluralsight.northwind.trading.UI;
 
-public class UserInterface {
+import com.pluralsight.northwind.trading.Data.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserInterface implements CommandLineRunner {
+    @Autowired
+    private ProductDao productDao;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hand in the Cookie Cutter");
+
+    }
 }
