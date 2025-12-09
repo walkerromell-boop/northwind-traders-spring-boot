7cadae51553fd3886fa8e6f88597d7c996f8cf6e
Romell Walker <walkerromell@gmail.com>
2025-12-09 12:12:35 -0500

    added beans

--- src/main/java/com/pluralsight/northwind/trading/NorthWindTradersSpringBootApplication.java
+++ src/main/java/com/pluralsight/northwind/trading/NorthWindTradersSpringBootApplication.java
@@ -2,12 +2,26 @@ package com.pluralsight.northwind.trading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class NorthWindTradersSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NorthWindTradersSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(NorthWindTradersSpringBootApplication.class, args);
 
+// Logging code to show you what Spring Boot is actually doing
+        String[] beanNames = context.getBeanDefinitionNames();
+        System.out.println("\nMy Application Beans:");
+        for (String beanName : beanNames) {
+            Object bean = context.getBean(beanName);
+            String packageName = bean.getClass().getPackageName();
+            if (packageName.startsWith("com.pluralsight.northwind.trading")) {
+                System.out.println(beanName + " -> " + bean.getClass().getSimpleName());
+            }
+        }
+
+    }
 }
